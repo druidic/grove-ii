f08b5cfb11044e138f190a6b03e376b91384f98c
Ben Christel <bchristel@pivotal.io>
2018-01-21 08:43:56 -0800

    Bump cache version

--- caching-service-worker.js
+++ caching-service-worker.js
@@ -25,7 +25,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-var version = 'Grove II Cache v5',
+var version = 'Grove II Cache v6',
     filesToCache = [
       // HTML
       './index.html',
